aead424c6d8b8c6adb953f2f8fb5d848efc3bc23
maleicacid <kazukuro0824+dev@gmail.com>
2023-04-29 00:15:46 +0900

    Debian package

---
+++ mkdeb.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -CEuo pipefail
+SCRIPT_DIR=$(cd $(dirname $0) && pwd)
+
+# Prep
+#apt install dkms dpkg make unzip --no-install-recommends -y
+cd $SCRIPT_DIR/fwtool
+make
+wget http://plex-net.co.jp/plex/pxw3u4/pxw3u4_BDA_ver1x64.zip -O pxw3u4_BDA_ver1x64.zip
+unzip -oj pxw3u4_BDA_ver1x64.zip pxw3u4_BDA_ver1x64/PXW3U4.sys && rm pxw3u4_BDA_ver1x64.zip
+./fwtool PXW3U4.sys it930x-firmware.bin && rm PXW3U4.sys
+
+cd ..
+mkdir -p ./installer/lib/firmware
+cp ./fwtool/it930x-firmware.bin ./installer/lib/firmware
+
+dkms mkdeb
+dpkg-deb --build installer
+mv ./installer.deb ../
+
