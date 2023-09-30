d10120f86d6da8cf89146eeec12cc47c1e0373f9
tsukumi <tsukumijima@users.noreply.github.com>
2023-09-30 03:13:24 +0000

    v0.4.1

--- winusb/src/BonDriver_PX4/resource.h
+++ winusb/src/BonDriver_PX4/resource.h
@@ -4,11 +4,11 @@
 
 #include <windows.h>
 
-#define VER_FILE	0,4,0,0
-#define VER_FILE_STR	"0.4.0"
+#define VER_FILE	0,4,1,0
+#define VER_FILE_STR	"0.4.1"
 
-#define VER_PRODUCT	0,4,0,0
-#define VER_PRODUCT_STR	"0.4.0"
+#define VER_PRODUCT	0,4,1,0
+#define VER_PRODUCT_STR	"0.4.1"
 
 #define VER_COMMENTS_STR		""
 #define VER_COMPANYNAME_STR		"nns779"
