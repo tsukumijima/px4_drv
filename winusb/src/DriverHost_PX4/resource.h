a0015bf651040eac79b05c521f0d88611e7e39f3
tsukumi <tsukumijima@users.noreply.github.com>
2024-02-26 13:19:56 +0000

    v0.4.3

--- winusb/src/DriverHost_PX4/resource.h
+++ winusb/src/DriverHost_PX4/resource.h
@@ -4,11 +4,11 @@
 
 #include <windows.h>
 
-#define VER_FILE	0,4,2,0
-#define VER_FILE_STR	"0.4.2"
+#define VER_FILE	0,4,3,0
+#define VER_FILE_STR	"0.4.3"
 
-#define VER_PRODUCT	0,4,2,0
-#define VER_PRODUCT_STR	"0.4.2"
+#define VER_PRODUCT	0,4,3,0
+#define VER_PRODUCT_STR	"0.4.3"
 
 #define VER_COMMENTS_STR		""
 #define VER_COMPANYNAME_STR		"nns779"
