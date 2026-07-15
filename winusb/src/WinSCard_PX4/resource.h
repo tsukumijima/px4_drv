d4b98d06cf530856b6f2c21b89e71236461706de
tsukumi <tsukumijima@users.noreply.github.com>
2026-07-16 06:14:02 +0900

    winusb: ビルド後の WinSCard.dll を識別できるよう rc ファイルを追加

---
+++ winusb/src/WinSCard_PX4/resource.h
@@ -0,0 +1,25 @@
+// resource.h
+
+#pragma once
+
+#include <windows.h>
+#include "../common/version.h"
+
+#define VER_COMMENTS_STR		""
+#define VER_COMPANYNAME_STR		"nns779"
+#define VER_FILEDESCRIPTION_STR		"WinSCard compatibility DLL for PX4 built-in card readers"
+#define VER_INTERNALNAME_STR		"WinSCard.dll"
+#define VER_LEGALCOPYRIGHT_STR		""
+#define VER_LEGALTRADEMARKS_STR		""
+#define VER_ORIGINALFILENAME_STR	VER_INTERNALNAME_STR
+#define VER_PRIVATEBUILD_STR		""
+#define VER_PRODUCTNAME_STR		"WinSCard_PX4"
+#define VER_SPECIALBUILD_STR		""
+
+#if defined(_DEBUG)
+#define VER_FLAGS	VS_FF_PRERELEASE | VS_FF_DEBUG
+#elif defined(_DEBUG_MSG)
+#define VER_FLAGS	VS_FF_PRERELEASE
+#else
+#define VER_FLAGS	0
+#endif
