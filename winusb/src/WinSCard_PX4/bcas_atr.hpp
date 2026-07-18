e1e8a1bd3a11309cebd7fa518dbb0743fbe34ba7
tsukumi <tsukumijima@users.noreply.github.com>
2026-07-19 04:01:45 +0900

    winusb: B-CAS の ATR についてよくわからんけど2種類あるらしい？のが謎なのでコメントを残す

--- winusb/src/WinSCard_PX4/bcas_atr.hpp
+++ winusb/src/WinSCard_PX4/bcas_atr.hpp
@@ -8,7 +8,7 @@
 
 namespace px4::winscard {
 
-/* B-CAS として登録済みの2種類は履歴バイトが異なり、末尾の TCK も連動して変わる */
+/* B-CAS として登録済みの2種類は履歴バイトが異なり、末尾の TCK も連動して変わる (TODO: 本当かどうか不明だが実際そうらしい) */
 inline constexpr std::array<std::array<BYTE, 13>, 2> BCAS_ATRS = {{
 	{ 0x3b, 0xf0, 0x12, 0x00, 0xff, 0x91, 0x81, 0xb1,
 		0x7c, 0x45, 0x1f, 0x01, 0x9b },
