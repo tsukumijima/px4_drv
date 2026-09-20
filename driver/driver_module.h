6238187cbd2f334aec55d078fe58eaac7c255125
tsukumi <tsukumijima@users.noreply.github.com>
2026-09-20 13:43:08 +0900

    v0.6.0

--- driver/driver_module.h
+++ driver/driver_module.h
@@ -3,6 +3,6 @@
 #ifndef __DRIVER_MODULE_H__
 #define __DRIVER_MODULE_H__
 
-#define PX4_DRV_VERSION	"0.5.6"
+#define PX4_DRV_VERSION	"0.6.0"
 
 #endif
--- winusb/src/common/version.h
+++ winusb/src/common/version.h
@@ -2,8 +2,8 @@
 
 #pragma once
 
-#define VER_FILE	0,5,6,0
-#define VER_FILE_STR	"0.5.6"
+#define VER_FILE	0,6,0,0
+#define VER_FILE_STR	"0.6.0"
 
-#define VER_PRODUCT	0,5,6,0
-#define VER_PRODUCT_STR	"0.5.6"
+#define VER_PRODUCT	0,6,0,0
+#define VER_PRODUCT_STR	"0.6.0"
